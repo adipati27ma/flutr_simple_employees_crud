85298349ffebf79aa6369ecac555c024b6e13ff3
adipati27ma <adipati.cs.samsung@gmail.com>
2024-03-30 13:29:21 +0700

    feat: add Firebase using FULL CLI!

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
-// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutr_simple_employees_crud/pages/home.dart';
 import 'package:flutter/material.dart';
 
+// Firebase dependency
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   /*
     docs: WidgetsFlutterBinding.ensureInitialized() used,
     this is a common practice in Flutter applications,
